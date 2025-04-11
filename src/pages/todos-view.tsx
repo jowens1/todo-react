4113644816beb25a38b17caaf078a00d951f96ab
jowens1 <justinowens328@gamail.com>
2025-04-10 17:14:24 -0700

    todo-view ground work

---
+++ src/pages/todos-view.tsx
@@ -0,0 +1,14 @@
+import Container from "../components/container"
+import Layout from "../components/layout"
+
+const TodosView = () => {
+    return (
+      <Layout>
+        <Container>
+            {'hello world'}
+        </Container>
+      </Layout>
+    )
+}
+
+export default TodosView
