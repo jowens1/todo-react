5952198453dfdfe2ebd50f48eb05ccd9a40d4a45
jowens1 <justinowens328@gamail.com>
2025-04-21 13:25:36 -0700

    todo list

--- src/components/todoForm.tsx
+++ src/components/todoForm.tsx
@@ -1,9 +1,16 @@
-import Input from "./input";
-
 const TodoForm = () => (
-  <>
-    <Input />
-  </>
-);
+    <form action="">
+      <div className="">
+        <label className="justify-items-center">Enter your next task:</label>
+        <input className="border h-8 rounded indent-2" />
 
+        <button
+          type="button"
+          className="bg-green-100 rounded-lg hover:bg-green-200 p-1 justify-items-center"
+        >
+          Add task
+        </button>
+      </div>
+    </form>
+);
 export default TodoForm;
--- src/pages/todosView.tsx
+++ src/pages/todosView.tsx
@@ -1,11 +1,15 @@
 import Container from "../components/container";
 import Layout from "../components/layout";
 import TodoForm from "../components/todoForm";
+import TodoList from "../components/todoList";
 const TodosView = () => {
   return (
     <Layout>
       <Container>
         <TodoForm />
+      </Container>
+      <Container>
+        <TodoList />
       </Container>
     </Layout>
   );
