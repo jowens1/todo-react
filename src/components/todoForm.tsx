1a3951298594b04bf3eddd464eba76913b1279fa
jowens1 <justinowens328@gmail.com>
2025-04-22 15:16:00 -0700

    Add todo Item and Add item to Todos (#10)
    Co-authored-by: jowens1 <justinowens328@gamail.com>

--- src/components/todoForm.tsx
+++ src/components/todoForm.tsx
@@ -1,16 +1,27 @@
-const TodoForm = () => (
-    <form action="">
-      <div className="">
-        <label className="justify-items-center">Enter your next task:</label>
-        <input className="border h-8 rounded indent-2" />
+type Props = {
+  newTodo: string;
+  setNewTodo: (value: string) => void;
+  setTodos: () => void;
+};
 
-        <button
-          type="button"
-          className="bg-green-100 rounded-lg hover:bg-green-200 p-1 justify-items-center"
-        >
-          Add task
-        </button>
-      </div>
-    </form>
-);
+const TodoForm = ({ newTodo, setNewTodo, setTodos }: Props) => {
+  return (
+    <div className="">
+      <label className="justify-items-center">Enter your next task:</label>
+      <input
+        className="border h-8 rounded indent-2"
+        value={newTodo}
+        onChange={(e) => setNewTodo(e.target.value)}
+      />
+
+      <button
+        type="button"
+        className="bg-green-100 rounded-lg hover:bg-green-200 p-1 justify-items-center"
+        onClick={setTodos}
+      >
+        Add task
+      </button>
+    </div>
+  );
+};
 export default TodoForm;
--- src/pages/todosView.tsx
+++ src/pages/todosView.tsx
@@ -1,15 +1,29 @@
+import { useState } from "react";
 import Container from "../components/container";
 import Layout from "../components/layout";
 import TodoForm from "../components/todoForm";
 import TodoList from "../components/todoList";
+import { TodoItem } from "../types/todo-types";
+
 const TodosView = () => {
+  const [todos, setTodos] = useState<TodoItem[]>([]);
+  const [newTodo, setNewTodo] = useState("");
+
+  const handleAddTodo = () => {
+    setTodos([...todos, { task: newTodo, completed: false }]);
+  };
+
   return (
     <Layout>
       <Container>
-        <TodoForm />
+        <TodoForm
+          newTodo={newTodo}
+          setNewTodo={setNewTodo}
+          setTodos={handleAddTodo}
+        />
       </Container>
       <Container>
-        <TodoList />
+        <TodoList todos={todos} />
       </Container>
     </Layout>
   );
