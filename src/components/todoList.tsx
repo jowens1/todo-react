05362110fc51754d47ca7692d50c98691f428bc6
jowens1 <justinowens328@gamail.com>
2025-04-22 15:14:22 -0700

    Add todo Item and Add item to Todos

--- src/components/todoList.tsx
+++ src/components/todoList.tsx
@@ -1,6 +1,17 @@
-const TodoList = () => {
-  const tempTodoList = ["First", "Second", "Third"];
-  return <div>{"TodoList"}</div>;
+import { TodoItem } from "../types/todo-types";
+
+type Props = {
+  todos: TodoItem[];
+};
+const TodoList = ({ todos }: Props) => {
+  const renderTodos = () => {
+    return todos.map((todo, index) => <li key={index}>{todo.task}</li>);
+  };
+  return (
+    <div>
+      <ul>{renderTodos()}</ul>
+    </div>
+  );
 };
 
 export default TodoList;
